1097d2fa2ab14dda76f344847b7dfd8a37875f72
Jide Tireni <jtirenipraise@gmail.com>
2025-02-02 20:12:08 +0100

    main go file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"num_class_api/handlers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
+
+	handlers.RegisterRoutes(server)
+	server.Run(":8080")
+
+}
+
+// func funfact(n int) (string, error) {
+// 	funfact, err := utils.GetFuncFact(n)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return funfact, nil
+// }
